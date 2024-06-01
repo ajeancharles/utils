9d3ff25ee0b461ba88f16358c1674901da443193
Arald Jean-Charles <araldjeancharles@verizon.net>
2024-05-31 22:00:28 -0400

    new file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utils',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple example package',
+    author='arald jean-charles',
+    author_email='your.email@example.com'
+)
